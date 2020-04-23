80466317fac0832c1b1ba5ecab3043355adfd8c7
Luqmanul Hakim <saya789.mn@gmail.com>
2020-04-23 07:04:27 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+alias config="git --git-dir='$HOME/dotfiles.git' --work-tree=$HOME"
+
 curl -sLf https://spacevim.org/install.sh | bash
 
 config config --local status.showUntrackedFiles no
