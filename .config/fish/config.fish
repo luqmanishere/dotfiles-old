4872ad3e4a4823aa26a4227af28bad85cf0468d5
Luqmanul Hakim <saya789.mn@gmail.com>
2020-05-23 15:20:04 +0800

    Integrated fisher packages

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -17,3 +17,9 @@ alias config="/usr/bin/git --git-dir=$HOME/dotfiles.git/ --work-tree=$HOME"
 alias ls="/usr/bin/exa"
 
 abbr pacinstall "sudo pacman -S --needed"
+
+if not functions -q fisher
+    set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME ~/.config
+    curl https://git.io/fisher --create-dirs -sLo $XDG_CONFIG_HOME/fish/functions/fisher.fish
+    fish -c fisher
+end
