0a0844688483abe122e191d0ec94c73fc2718633
Luqmanul Hakim <saya789.mn@gmail.com>
2020-11-18 17:09:44 +0800

    Fixed stupid fisher

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -20,15 +20,18 @@ alias mux tmuxinator
 abbr pacinstall "sudo pacman -S --needed"
 abbr pacupdate "sudo pacman -Syu"
 
+starship init fish | source
 # install fisher if not installed
 if not functions -q fisher
     set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME ~/.config
-    curl https://git.io/fisher --create-dirs -sLo $XDG_CONFIG_HOME/fish/functions/fisher.fish
-    fish -c fisher
+    cd ~/.config/fish/functions && wget git.io/fisher
+    mv fisher fisher.fish
+    source fisher.fish && fisher update
+    cd
+    fish
 end
 
 # start ssh agent
 fish_ssh_agent
 
 # initialize starship prompt
-starship init fish | source
